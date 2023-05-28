77d6f6d9780bbc80bdb18546761df25efe71dcbb
ParasMarios <paraske.marios@gmail.com>
2023-05-28 11:44:29 +0300

    change the navigate url.

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -19,7 +19,7 @@ export default function Register() {
         email,
         password,
       });
-      navigate("/app/home");
+      navigate("/app/students");
     } catch (error) {
       console.error("Error during registration", error);
       alert("Failed to register. Please check your information and try again.");

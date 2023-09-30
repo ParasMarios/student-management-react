aa87e035a5339f82f7fbad1230dfe401c27237d5
ParasMarios <paraske.marios@gmail.com>
2023-09-30 19:17:09 +0300

    Add comment for Configuring the Backend URL

--- src/axiosInstance.jsx
+++ src/axiosInstance.jsx
@@ -1,6 +1,36 @@
 import axios from "axios";
 
-// Determine the backend URL based on the current environment
+/**
+ * Configuring the Backend URL
+ *
+ * By default, the URL is set to "http://localhost:8080/api/v1", which is suitable for
+ * local development. However, you should update this URL to match your specific backend
+ * server's address when deploying your application to different environments.
+ *
+ * Configuration Steps:
+ *
+ * 1. Locate the `backendUrl` variable in your code:
+ *    ```
+ *    const backendUrl =
+ *      process.env.REACT_APP_BACKEND_URL || "http://localhost:8080/api/v1";
+ *    ```
+ *
+ * 2. Update the `backendUrl` variable as follows:
+ *    - For local development, you can leave it as is (http://localhost:8080/api/v1).
+ *    - For other environments, replace it with the appropriate
+ *      backend server URL. For example:
+ *      ```
+ *      const backendUrl = "https://your-api-server.com/api/v1";
+ *      ```
+ *
+ * 3. Save the changes to your code.
+ *
+ * By following these steps, you can configure the backend URL for your React application
+ * based on your specific environment.
+ *
+ * Make sure to replace the URLs with the actual backend server URLs you want to use
+ * in each environment.
+ */
 const backendUrl =
   process.env.REACT_APP_BACKEND_URL || "http://localhost:8080/api/v1";
 

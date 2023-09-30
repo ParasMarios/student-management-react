a4173984cfc1d15a93b7163993c3a13458e72c59
ParasMarios <paraske.marios@gmail.com>
2023-09-30 18:32:04 +0300

    Display date and time when a change is made

--- src/students/DetailsStudent.jsx
+++ src/students/DetailsStudent.jsx
@@ -12,9 +12,16 @@ export default function DetailsStudent() {
         const result = await axiosInstance.get(
           `/students/${encodeURIComponent(email)}`
         );
+        console.log("Received student data:", result.data);
         setStudent({
           ...result.data,
         });
+
+        if (result.data && result.data.lastModifiedDate) {
+          setStudent({
+            ...result.data,
+          });
+        }
       } catch (error) {
         console.error("Error while fetching student:", error);
       }
@@ -61,6 +68,22 @@ export default function DetailsStudent() {
           <strong>Thesis Title:</strong> {student.thesisTitle}
         </p>
         <div>
+          {student.lastModifiedDate ? (
+            <p>
+              <strong>Last Modified Date:</strong>{" "}
+              {new Date(student.lastModifiedDate).toLocaleString("en-US", {
+                year: "numeric",
+                month: "long",
+                day: "numeric",
+                hour: "2-digit",
+                minute: "2-digit",
+                second: "2-digit",
+              })}
+            </p>
+          ) : (
+            <p>No last modified date available.</p>
+          )}
+
           <strong>Comments:</strong>
           {student.comments && student.comments.length > 0 ? (
             <ul>
